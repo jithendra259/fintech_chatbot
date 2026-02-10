b206d213f1a66ddf2204f853857411aa5470bd72
jithendra259 <jithendrasubramanyam218@gmail.com>
2026-02-11 01:23:43 +0530

    optimisation test complete

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from agents.data_agent import DataFetchAgent
 from config.config import config
 from agents.data_alignment_agent import DataAlignmentAgent
+from agents.optimization_agent import optimisationagent as OptimizationAgent
 
 prices = DataFetchAgent.run(
     assets=config.assets,
@@ -17,3 +18,17 @@ print("prices",aligned["prices"].head())
 print(aligned["returns"].head())
 print(aligned["mean_returns"])
 print(aligned["covariance"].shape)
+
+# Step 3: Optimize portfolio
+result = OptimizationAgent.run(
+    aligned_data=aligned,
+    risk_aversion = config.risk_aversion
+
+)
+
+print("Optimal Weights:")
+for k, v in result["weights"].items():
+    print(f"{k}: {v:.4f}")
+
+print("\nExpected Return:", result["expected_return"])
+print("Portfolio Risk:", result["risk"])
