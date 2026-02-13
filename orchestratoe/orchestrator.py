4936743437b5ce2755d9c5f8ee6bac4027b3e4e6
jithendra259 <jithendrasubramanyam218@gmail.com>
2026-02-13 14:26:38 +0530

    Refactored orchestrator and restored stashed changes

--- orchestratoe/orchestrator.py
+++ orchestratoe/orchestrator.py
@@ -1,39 +1,38 @@
+from agents.data_agent import DataFetchAgent
+from agents.data_alignment_agent import DataAlignmentAgent
+from agents.optimization_agent import optimisationagent as OptimizationAgent
+from agents.ai_reasoning_agent import AIReasoningAgent
 
-from agents.data_agent import datafetchagent
-from agents.data_alignment_agent import Dataalignmentagent
-from agents.optimization_agent import optimisationagent
-from agents.ai_reasoning_agent import aireasoningagent
 
-class orchestrator:
-    def __init__(self,config):
-        self.config=config
+class Orchestrator:
+    def __init__(self, config):
+        self.config = config
 
     def run_pipeline(self):
-        # fetch raw price data 
-        raw_data=datafetchagent.run(
+
+        # Step 1: Fetch raw price data
+        raw_data = DataFetchAgent.run(
             assets=self.config.assets,
             start_date=self.config.start_date,
             end_date=self.config.end_date,
-            frequency=self.config.frequency
+            frequency=self.config.data_frequency
         )
 
-        #align data and compute statistics 
+        # Step 2: Align data and compute statistics
+        aligned_data = DataAlignmentAgent.run(raw_data)
 
-        aligned_data=Dataalignmentagent.run(raw_data)
+        # Step 3: Portfolio optimization
+        portfolio_result = OptimizationAgent.run(
+            aligned_data=aligned_data,
+            risk_aversion=self.config.risk_aversion
+        )
 
-        #run classical portifoio optimisation
-
-        portfolio_result=optimisationagent.run(aligned_data,
-                            risk_aversion=self.config.risk_aversion
-                                               )
-        #Generate ai based explanations and insights 
-        explanations=aireasoningagent.run(
-            portfolio_result,
-            )
-        
-        #return structure output 
+        # Step 4: AI Explanation
+        explanation = AIReasoningAgent.run(portfolio_result)
 
         return {
-            "portfolio result": portfolio_result,
-            "explanations": explanations
+            "raw_data": raw_data,
+            "aligned_data": aligned_data,
+            "portfolio_result": portfolio_result,
+            "explanation": explanation
         }
